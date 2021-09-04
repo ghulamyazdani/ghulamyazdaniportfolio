2c6593766fa7045582a2e4def6707038405063fb
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-09-04 15:08:54 +0530

    First commit

---
+++ src/components/about.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>Driven undergrad Student currently in third year of my college with exceptional communication and analytical skills. Effectively articulates ideas with poise and confidence to engage prospective students. Strong knowledge of campus activities, clubs and culture. I am the person who is good at web designing and fun fact is that i made my first website at the age of 17 . which is up and running till now .Obviously I have made some changes in it but making a website at the age of 17 always amaze people when i tell them. .You can check it here- proleaker.com,. I have Very strong knowledge of firebase and I have frontend development skill as well.I have participated in so many Hackathons and worked on so many projects and created so many connections with so many people who are having same goals like mine .I am face(campus ambassador) of some big companies like Codingblocks,Codingninjas and prepbytes.</p>
+                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Web Designing</h3>
+                    <p>I have good grasp over fundamental concepts of Web Designin.Tools I use are Figma,Xd,Photoshop,Illustrator.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Cybersecurity</h3>
+                    <p>I have lots of knowledge in cybersecurity and have done an internship with Gurugram police as well.</p>
+                </div>
+                </div>
+            </div>
+            {/*
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>Jenkins , Kubernetes , Docker </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div>
+            */}
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
